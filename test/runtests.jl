e2c86f34056baf4e0def309d1b53e867406c3031
Beforerr <zzj956959688@gmail.com>
2025-12-23 20:05:36 -0800

    CI: update result test

--- test/runtests.jl
+++ test/runtests.jl
@@ -35,5 +35,32 @@ end
     # Solve using matrix eigenvalue method
     # J=12 provides good accuracy (J-pole approximation order)
     ωs = solve_kinetic_dispersion(species, B0, kx, kz; N = 6, J = 12)
-    @test filter(ω -> isfinite(ω) && imag(ω) > 0.001 * wce, ωs) ./ wce ≈ [0.622724595851375 + 0.15724974983302017im]
+    @test filter(ω -> isfinite(ω) && imag(ω) > 0.001 * wce, ωs) ./ wce ≈ [0.6229225524157314 + 0.15686544364848592im]
+end
+
+
+@testset "Astfalk 2017 firehose instability" begin
+    using PlasmaBO
+    using PlasmaBO: q, kb, ε0, me, c0, mp
+    using MAT: matopen
+
+    B0 = 0.1
+    θ = deg2rad(45)
+    n = 5.0e19
+
+    me_mp = 1 / 1836 # [proton mass]
+    electron = Maxwellian(-1.0, me_mp, n, 496.683)
+    fpath = pkgdir(PlasmaBO, "test/firehose_Astfalk17_fvceff1.mat")
+    proton_param = matopen(fpath) do file
+        fvc = read(file, "fvc")
+        HHSolverParam(q, mp, n, B0, fvc["vtz"], fvc["vtp"], 0.0, 0.0, fvc["alm"])
+    end
+
+    kn = 31.0613
+    k = kn / 4
+    wci = proton_param.wc
+    species = (proton_param, electron)
+    ωs = solve_kinetic_dispersion(species, B0, k .* sincos(θ)...; N = 2, J = 24)
+    ω_unstable = filter(ω -> isfinite(ω) && imag(ω) > 0.001 * wci, ωs)
+    @test imag.(ω_unstable ./ wci) ≈ [0.062373877285804444] rtol = 1.0e-3
 end
