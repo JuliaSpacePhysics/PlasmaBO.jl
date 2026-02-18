edb52fab6ea4a40282d1de2efcf6b827027cb6b9
Hongyang Zhou <hyzhou@umich.edu>
2026-02-18 17:14:04 -0500

    refactor: use constants from Unitful

--- src/constants.jl
+++ src/constants.jl
@@ -1,12 +1,15 @@
 # Physical constants
 module Constants
+using Unitful
+
 export c0, q, mp, me, kb, ε0, μ0, qe
-const c0 = 299_792_458  # m/s
-const q = 1.602_176_634e-19     # C
-const mp = 1.672_621_923_69e-27      # kg
-const me = 9.109_383_7015e-31         # kg
-const kb = 1.380_649e-23     # J/K
-const ε0 = 8.8541878188e-12  # F/m
-const μ0 = 1.0 / (c0^2 * ε0)  # H/m
+
+const c0 = ustrip(upreferred(Unitful.c0))
+
+for f in (:μ0, :ε0, :q, :k, :me, :mp)
+    @eval const $f = ustrip(upreferred($Unitful.$f))
+end
+
+const kb = k
 const qe = q
 end
