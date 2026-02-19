dd1b23649c53ca5e739a589cc07c12e6dbf237b2
Hongyang Zhou <hyzhou@umich.edu>
2026-02-18 20:56:04 -0500

    Drop upreferred

--- src/constants.jl
+++ src/constants.jl
@@ -4,10 +4,11 @@ using Unitful
 
 export c0, q, mp, me, kb, ε0, μ0, qe
 
-const c0 = ustrip(upreferred(Unitful.c0))
+const c0 = Unitful.c0.val
+const ε0 = uconvert(Unitful.F / Unitful.m, Unitful.ε0).val
 
-for f in (:μ0, :ε0, :q, :k, :me, :mp)
-    @eval const $f = ustrip(upreferred($Unitful.$f))
+for f in (:μ0, :q, :k, :me, :mp)
+    @eval const $f = ($Unitful.$f).val
 end
 
 const kb = k
